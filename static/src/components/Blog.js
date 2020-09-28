2b1f374f72c096978e0bb14094847fc134524bdc
Erik Lintunen <erik.lintunen@network.rca.ac.uk>
2020-09-28 13:31:23 +0100

    Add system for generating blog entries

--- static/src/components/Blog.js
+++ static/src/components/Blog.js
@@ -8,8 +8,6 @@ import { sequencer } from '../Utils'
 class Blog {
   constructor(container) {
 
-    // TO DO: Devise system to algorithmically generate content
-    // but need to include a way to load and order links, embeds, media...
     // Currently missing (decide whether to archive or include in live build):
     // 140520: The expressive power of the (digital) line
     // 270420: A case for postdramatic theatre
@@ -17,19 +15,45 @@ class Blog {
     // Generate content but skip first object (template).
     for (let i = 1; i < text.blog.length; i++) {
 
+      // Post title (date: header).
       const title = document.createElement('div')
       title.classList.add('blogTitle')
       title.innerText = text.blog[i].date + ': ' + text.blog[i].title
       content.appendChild(title)
 
+      // Main body.
       const body = document.createElement('div')
       body.classList.add('blogBody')
       content.appendChild(body)
 
+      // Generate individual paragraphs.
       for (let j = 0; j < text.blog[i].body.length; j++) {
+
+        // Text.
         const paragraph = document.createElement('p')
         paragraph.innerHTML = text.blog[i].body[j]
         body.appendChild(paragraph)
+
+        // If text includes links.
+        if (text.blog[i].links.length > 0) {
+          // Replace markers for links and descriptions.
+          for (let k = 0; k < text.blog[i].links.length; k++) {
+            const link = Object.values(text.blog[i].links[k])
+            const description = Object.keys(text.blog[i].links[k])
+            const replacement = paragraph.innerHTML.replace('[' + (k + 1) + ']', '<a href="' + link + '">' + description + '</a>')
+            paragraph.innerHTML = replacement
+          }
+        }
+
+      }
+
+      // Include embeds if any.
+      if (text.blog[i].embeds.length > 0) {
+        for (let j = 0; j < text.blog[i].embeds.length; j++) {
+          const paragraph = document.createElement('p')
+          paragraph.innerHTML = text.blog[i].embeds[j]
+          body.appendChild(paragraph)
+        }
       }
 
     }
